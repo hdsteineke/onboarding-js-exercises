dd70ee260667914cbd6d9d3f6041eb8a1e42a269
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-02-28 15:01:27 -0800

    solved three

--- app.js
+++ app.js
@@ -6,20 +6,31 @@
 // a number of minutes as an argument and returns
 // the number of seconds
 
-function convertToSeconds() {}
+function convertToSeconds(minutes) {
+    let seconds = minutes * 60;
+    return seconds;
+}
 
 console.log(`convertToSeconds(10) === 600 : ${convertToSeconds(10) === 600}`);
 
 // 2. areaOfTriangle
 // Write a function called areaOfTriangle that takes in the base and height
 // of a triangle and returns its area
-function areaOfTriangle() {}
+function areaOfTriangle(base, height) {
+    let area = (base * height) / 2;
+    return area;
+}
 console.log(`areaOfTriangle(5, 10) === 25 : ${areaOfTriangle(5, 10) === 25}`);
 
 // 3. lessThan100
 // Given 2 numbers, return true if their sum is less than 100
 // return false if it is greater than 100
-function lessThan100() {}
+function lessThan100(num1, num2) {
+    if (num1 + num2 < 100)
+    return true;
+    else return false;
+
+}
 console.log(`lessThan100(5, 10) === true : ${lessThan100(5, 10) === true}`);
 console.log(`lessThan100(100, 10) === false : ${lessThan100(100, 10) === false}`);
 
